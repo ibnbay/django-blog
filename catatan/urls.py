adac86f8ee6044049563e183a7763e5dc7a5c0d7
ibnbay <sky89@live.com>
2015-12-27 20:01:56 +0700

    Add Detail Page

--- catatan/urls.py
+++ catatan/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-import catatan.views
+from . import views
 
 urlpatterns = [
-    url(r'^$', catatan.views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- catatan/views.py
+++ catatan/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
 from .models import Post
+
+
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'catatan/post_list.html', {'list_post': posts})
+
+# def post_detail(request, post_id):
+#     posts = Post.objects.get(pk=post_id)
+#     return render(request, 'catatan/post_detail.html', {'list_post': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'catatan/post_detail.html', {'post': post})
